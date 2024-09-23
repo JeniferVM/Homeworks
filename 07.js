d4335096d69cba9debe9b28d7e80c543ba66e8e0
JeniferVM <villanueva.moran.21@gmail.com>
2024-09-22 20:50:37 -0600

    Add files via upload

---
+++ 07.js
@@ -0,0 +1,10 @@
+function esTipoDato(valor) {
+  // La función recibe un argumento llamado valor.
+  // Retorna el tipo de dato de este valor.
+  // Por ejemplo: "string", "number", "boolean", "object", etc.
+  // Tu código:
+  const tipo = typeof valor;
+  return tipo
+}
+
+module.exports = esTipoDato; 
---
+++ 10.js
@@ -0,0 +1,14 @@
+function devolverString(string) {
+  // La función recibe un argumento llamado string.
+  // Debe retornar dicho string.
+  // Por ejemplo: 
+  // "texto" ---> "texto" 
+  // "hola mundo" ---> "hola mundo" 
+  // "SoyHenry" ---> "SoyHenry" 
+  // Tu código:
+  if (string === string); {
+    return string
+  }
+
+}
+module.exports = devolverString;
